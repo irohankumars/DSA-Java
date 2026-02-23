82686f81f6f9189834629ce9da07db0478ffb595
irohankumars <kumarsrohan2@gmail.com>
2026-02-23 20:26:04 +0530

    Finding area of the square

--- JavaBasics.java
+++ JavaBasics.java
@@ -5,12 +5,10 @@ public class JavaBasics {
 
         Scanner sc = new Scanner(System.in);
        // average of 3 numbers
-            int a = 20;
-            int b= 29;
-            int c = 322;
-            double avg = (a +b+c)/3;
-            System.out.println(avg);
-     
+        System.out.print("Enter the side of Square to find out the Area: ");
+        int side = sc.nextInt();
+        int area = side * side;
+        System.out.println("Area of the square is: " + area);
       
     }
 }
