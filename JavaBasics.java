f172861857a1a73e12a8d100818c1cad5c840fa1
irohankumars <kumarsrohan2@gmail.com>
2026-02-23 20:22:14 +0530

    type coversion

--- JavaBasics.java
+++ JavaBasics.java
@@ -4,9 +4,13 @@ public class JavaBasics {
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("input radius:");
-        int rad = sc.nextInt();
-        float area = 3.14f * rad * rad;
-        System.out.println(area);
+       // average of 3 numbers
+            int a = 20;
+            int b= 29;
+            int c = 322;
+            double avg = (a +b+c)/3;
+            System.out.println(avg);
+     
+      
     }
 }
