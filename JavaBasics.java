39d7c992aa196c3cb43cc20415425c62ca198d95
irohankumars <kumarsrohan2@gmail.com>
2026-02-24 19:28:12 +0530

    Operators

--- JavaBasics.java
+++ JavaBasics.java
@@ -5,14 +5,21 @@ public class JavaBasics {
 
         Scanner sc = new Scanner(System.in);
        // average of 3 numbers
-        float pen = 12.0f;
-        float pencil = 112.0f;
-        float paper = 120.0f;
-float Subtotal = pen + pencil  + paper;
-float tax = Subtotal * 0.18f;
-float Total = Subtotal + tax;
-System.out.println("Total cost with GST 18% is :: " + Total);
-      int $=24;
-      System.out.println($);
+//         float pen = 12.0f;
+//         float pencil = 112.0f;
+//         float paper = 120.0f;
+// float Subtotal = pen + pencil  + paper;
+// float tax = Subtotal * 0.18f;
+// float Total = Subtotal + tax;
+// System.out.println("Total cost with GST 18% is :: " + Total);
+//operating on operators
+    // System.out.println(!(1>3));
+//Assignment operators
+        int A = 10;
+        // System.out.println(A+=10);               
+        // System.out.println(A+=10);
+        System.out.println(A *= 10);
+        System.out.println(A /= 10);
+
     }
 }
