1082b0ad7aed385856ab1a3bf05fbb41cf5af703
irohankumars <kumarsrohan2@gmail.com>
2026-02-28 21:15:31 +0530

    Reverse

--- JavaBasics.java
+++ JavaBasics.java
@@ -181,6 +181,15 @@ public class JavaBasics {
                     int digit = num % 10;
                     System.out.print(digit + " ");
                     num2 /= 10; 
+
+
+
+                System.out.println("Enter the numbers: ");
+                int num2 = sc.nextInt();
+                while(num2 != 0){
+                    int digit = num % 10;
+                    System.out.print(digit + " ");
+                    num2 /= 10; 
                 }
     
         }
