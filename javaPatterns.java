d314972ce1b9da436b54ae7d6b9421a779434f32
irohankumars <kumarsrohan2@gmail.com>
2026-03-09 22:22:04 +0530

    patterns

---
+++ javaPatterns.java
@@ -0,0 +1,92 @@
+public class javaPatterns {
+    
+    public static void hollowRectangle(int totRow, int totCol){
+        for(int i =1 ; i<=totRow; i++){
+            for(int j=1; j<=totCol; j++){
+                if(i==1 || i==totRow || j==1 || j==totCol){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+
+            }
+        System.out.println();
+        }    
+}
+
+    public static void invertHalfPyramid(int n){
+        for(int i =1; i<= n; i++){
+            for(int j = 1; j<= n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+       public static void invertHalfPyramidupsidedown(int n){
+        for(int i =n ; i>= 1; i--){
+             for(int j=1; j<=i;j++){
+                System.out.print(j);
+            }
+            for(int j = 1; j<= i; j++){
+                System.out.print(" ");
+            }
+           
+            System.out.println();
+
+        }
+    }
+
+    //method 2
+    public static void invertHalfPyramid2(int n){
+        for(int i =1 ; i<= n; i++){
+             for(int j=1; j<=n-i+1;j++){
+                System.out.print(" "+j);
+            }
+            for(int j = 1; j<= i; j++){
+                System.out.print(" ");
+            }
+           
+            System.out.println();
+
+        }
+    }
+
+    public static void triangle(int n){
+        int num = 1;
+        for(int i = 1; i<=n; i++){
+            for(int j=1; j<= i;j++){
+                System.out.print(num + " ");
+                num ++;
+
+            }
+            System.out.println();
+        }
+    }
+
+      public static void triangle01(int n){
+        int num = 1;
+        for(int i = 1; i<=n; i++){
+            for(int j=1; j<= i;j++){
+                if((i+j) % 2==0){
+                    System.out.print(" "+1);
+                }else{
+                    System.out.print(" "+ 0);
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+
+ public static void main(String[] args) {
+    
+    // hollowRectangle(4, 20);
+    triangle01(5);
+    invertHalfPyramid(5);
+
+        }
+    }
