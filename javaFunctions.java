a958654cfafafc3e7080d86312c5c06277880af6
irohankumars <kumarsrohan2@gmail.com>
2026-03-09 18:41:10 +0530

    Functions

--- javaFunctions.java
+++ javaFunctions.java
@@ -53,15 +53,71 @@ public class javaFunctions {
 
   }
 
+  public static void avgThree(int n, int n1, int n2){
+  int avg = (n + n1 + n2) / 3;
+  System.out.println(" the avg of three numbers you entered is "+ avg);
+  }
+ 
+  public static void isEven(int n){
+    if(n % 2 == 0){
+      System.out.println("The Number is Even");
+    }else{
+      System.out.println("The Number is Odd");
+    }
+  }
+
+  public static void isPali(int number){
+    int copyNum = number;
+    int reverse = 0;
+    if(number < 0){
+      System.out.println("Enter positive integer only"); 
+    }else{
+      while(number != 0){
+      int digit = number % 10;
+      reverse = (reverse * 10 );
+      reverse  +=  digit;
+      number /= 10;
+    }
+
+    if(copyNum == reverse){
+      System.out.println("the Entered Number " + copyNum +" is Plaindrome");
+    }else{
+      System.out.println("Entered Number " + copyNum +" is not Palindrome");
+    }
+    }
+
+    
+  }
+
+  public static void sumOfDigits(int number){
+    int sum = 0;
+    if(number < 0){
+      System.out.println("Enter the Valid digit to add");
+    }else{
+      while(number != 0){
+        int digit = number % 10;
+        sum = sum + digit;
+        number /= 10;
+      }
+      System.out.println("the sum of the digits you enterd is " + sum);
+    }
+  }
   public static void main(String[] args) {
 
-    // Scanner sc = new Scanner(System.in);
-    // System.out.print("Enter the range: ");
-    // int n = sc.nextInt();
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a Number: ");
+    int n = sc.nextInt();
+    sumOfDigits(n);
+
+    // int n1 = sc.nextInt();
+    // int n2 = sc.nextInt();
+
+    // avgThree(n, n1, n2);
+
     // primeRange(n);    
 
     //binary to dec
-    binTodec(101);
-    decTobin(120);
+    // binTodec(101);
+    // decTobin(120);
   }
 }
