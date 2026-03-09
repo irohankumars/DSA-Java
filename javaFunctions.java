c519c829190e5e47413216161d40b3601a74a45b
irohankumars <kumarsrohan2@gmail.com>
2026-03-09 07:26:27 +0530

    binary conversion

--- javaFunctions.java
+++ javaFunctions.java
@@ -24,11 +24,44 @@ public class javaFunctions {
     
   }
 
+  public static void binTodec(int binNum){
+    int printNum = binNum;
+    int decNum = 0;
+    int pow = 0;
+
+    while(binNum > 0){
+      int lastDigit = binNum % 10;
+      decNum = decNum + (lastDigit * (int)Math.pow(2,pow));
+      pow++;
+      binNum = binNum / 10;
+    }
+    System.out.println("decimal form of "+printNum+" will be "+decNum);
+  }
+
+  public static void decTobin(int decNum){
+    int printNum = decNum;
+    int binNum = 0;
+    int pow = 0;
+
+    while(decNum>0){
+      int rem =   decNum % 2;
+      binNum = binNum + (rem * (int)Math.pow(10,pow));
+      pow++;
+      decNum =  decNum /2;
+    }
+    System.out.print("Binary form of "+printNum+" will be "+binNum);
+
+  }
+
   public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the range: ");
-    int n = sc.nextInt();
-  primeRange(n);    
+    // Scanner sc = new Scanner(System.in);
+    // System.out.print("Enter the range: ");
+    // int n = sc.nextInt();
+    // primeRange(n);    
+
+    //binary to dec
+    binTodec(101);
+    decTobin(120);
   }
 }
