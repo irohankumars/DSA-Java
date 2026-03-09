948b98fa1880a2d8dfed49ffb0ff7c2e2b3e1508
irohankumars <kumarsrohan2@gmail.com>
2026-03-10 05:28:07 +0530

    butterfly pattern

--- javaPatterns.java
+++ javaPatterns.java
@@ -82,11 +82,39 @@ public class javaPatterns {
         
     }
 
+    public static void butterfly(){
+        int n = 10;
+
+        for(int i =0; i<=n;i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i =n; i>=1;i--){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
  public static void main(String[] args) {
     
     // hollowRectangle(4, 20);
-    triangle01(5);
-    invertHalfPyramid(5);
+    butterfly();
 
         }
     }
