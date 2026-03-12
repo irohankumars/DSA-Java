43ce7bb5d8f4ec7d9d2d43f6bdd5fc5e19bb23b8
irohankumars <kumarsrohan2@gmail.com>
2026-03-12 20:50:08 +0530

    Triangles

--- patternPrac.java
+++ patternPrac.java
@@ -83,24 +83,72 @@ public class patternPrac {
         }
     }
 
-  public static void mirrorTriangle(){
-    int n = 4;
+    public static void mirrorTriangle(){
+        int n = 4;
 
-    for(int i = 1; i <= n; i++){
+        for(int i = 1; i <= n; i++){
 
-        for(int j = 1; j <= (n - i); j++){
-            System.out.print(" ");
+            for(int j = 1; j <= (n - i); j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
         }
-
-        for(int j = 1; j <= i; j++){
-            System.out.print("*");
-        }
-
-        System.out.println();
     }
-}
+
+        public static void mirrorTriangleInverted(){
+        int n = 4;
+
+        for(int i = n; i >= 1; i--){
+
+            
+            for(int j = 0; j <= n - i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+
+            
+
+            System.out.println();
+        }
+    }
+
+    public static void  centeredTriangle(){
+        int n = 4;
+        for(int i=1;i<=n;i++){
+            for(int j =1;j<=(n-i) ; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1 ;j <= 2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void  centeredTriangleInverted(){
+        int n = 4;
+        for(int i=n;i>=1;i--){
+           
+            for(int j =1;j<=(n-i) ; j++){
+                System.out.print(" ");
+            }
+             for(int j = 1 ;j <= 2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
+
+        centeredTriangleInverted();
      
         
     }
