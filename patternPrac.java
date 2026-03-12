8460fa65caa40a0469a25e01ad0c9fa62bde1e6b
irohankumars <kumarsrohan2@gmail.com>
2026-03-12 22:40:06 +0530

    Triangle

--- patternPrac.java
+++ patternPrac.java
@@ -195,9 +195,23 @@ public class patternPrac {
         }
     }
 
+    public static void triangleCentEven(){
+        int n = 4;
+        for(int i =1; i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+    }
+
     public static void main(String[] args) {
 
-        hollow_rectangle();
+        triangleCentEven();
      
         
     }
