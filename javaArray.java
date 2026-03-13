494a0de0c57cafc54d9e70e8057b11b49d3d7c27
irohankumars <kumarsrohan2@gmail.com>
2026-03-13 07:34:13 +0530

    Array

--- javaArray.java
+++ javaArray.java
@@ -1,5 +1,14 @@
 import java.util.*;
 public class javaArray {
+
+
+    public static int  update(int marks[], int nonChnageable){
+        
+        for(int i=0;i<= marks.length - 1;i++){
+            marks[i] = marks[i]+2;
+        }
+        return nonChnageable  =10;
+    }
     public static void main(String[] args) {
                 Scanner sc = new Scanner (System.in);
 
@@ -7,15 +16,18 @@ public class javaArray {
 
         System.out.println("Enter the number of subjects: ");
         int n = sc.nextInt();
-        for(int i = 1; i<=n ; i++){
+        for(int i = 1; i<=n; i++){
             System.out.print("Enter the marks of " + i +" Subject: ");
             int mark = sc.nextInt();
             marks[i] = mark;
         }
+        int nonChnageable = 5;
+        update(marks, nonChnageable);
 
-        System.out.println("marks entered");
-        for(int i=0;i<=n;i++){
-            System.out.print(marks[i]);
+        System.out.println(nonChnageable);
+        System.out.println("additional grace marks +2");
+        for(int i=1;i<=n;i++){
+            System.out.println("subject "+ i +" marks is :" +marks[i]);
         }
 
     sc.close();
