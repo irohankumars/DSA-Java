a45619f7be49ac84f6f6d70264cc384c1ab9f7c2
irohankumars <kumarsrohan2@gmail.com>
2026-03-16 21:11:50 +0530

    Binary Search Tree

--- searchTechniques.java
+++ searchTechniques.java
@@ -1,8 +1,6 @@
-// import java.util.*;
+import java.util.*;
 
-import java.util.Scanner;
-
-public class linearSearch {
+public class searchTechniques {
     
     public static void linearSearchArray(){
         Scanner sc = new Scanner(System.in);
@@ -50,10 +48,50 @@ public class linearSearch {
 
     }
 
+
+    public static int BSt(int arr[],int key){
+        // Scanner sc = new Scanner(System.in);
+
+        // System.out.print("Enter the number of elements in the array: ");
+        // int n = sc.nextInt();
+
+        // System.out.print("Enter the elements of the array: ");
+        // for(int i=0;i<=n-1;i++){
+        //     arr[i] = sc.nextInt();
+        // }
+        // System.out.println("The Elemets of the Array: "+ Arrays.toString(arr));
+        // Arrays.sort(arr);
+        // System.out.println("Sorted Array: " +  Arrays.toString(arr));
+        // System.out.print("Enter the key that needs to be searched: ");
+        // int key = 22;//sc.nextInt();
+
+        int start = 0, end = arr.length - 1;
+
+        while(start <= end){
+            int mid = (start + end)/2;
+
+            //comparisons
+            if(arr[mid] == key){
+                return mid;
+            }
+            if(arr[mid] < key){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         
         // linearSearch();
         // largestInArray();
+        int arr[] = {11,22,33,44,55,88,99};
+        int key = 88;
+        System.out.println("the index for key is: "+ BSt(arr,key));
+        
       
     }
 
