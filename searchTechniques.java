97e786bf7b7f7c02e5889fba740241c9968e214e
irohankumars <kumarsrohan2@gmail.com>
2026-06-21 11:11:38 +0530

    trapped water

--- searchTechniques.java
+++ searchTechniques.java
@@ -181,14 +181,17 @@ public class searchTechniques {
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
         int arr[] = {2,4,6,8,10};
+        int prefix[] = new int[arr.length];
+
+        prefix[0] = arr[0];
+        for(int i=1;i<prefix.length;i++){
+            prefix[i] = prefix[i-1]+ arr[i];
+        }
         for(int i=0;i<arr.length;i++){
             int start = i;
             for(int j=i+1;j< arr.length;j++){
                 int end = j;
-                currSum=0;
-                for(int k=start;k<=end;k++){
-                    currSum += arr[k];
-                }
+                currSum= start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
                 if(maxSum <currSum ){
                     maxSum = currSum;
                 }
@@ -198,8 +201,25 @@ public class searchTechniques {
         System.out.println("the max sum of sub array is: " + maxSum);
     }
    //new
-    public static void main(String[] args) {
-        
+
+
+
+public static void kadanes(int numbers []){
+    int ms = Integer.MIN_VALUE;
+    int cs = 0;
+
+    for(int i=0; i<numbers.length; i++) {
+        cs = cs + numbers [i] ;
+            if(cs <0){
+            cs = 0;
+            }
+    ms = Math.max(cs, ms);
+    }
+
+System.out.println("our max subarray sum is :"+ ms);
+}
+public static void main(String[] args) {
+        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
         // linearSearch();
         // largestInArray();
         // int arr[] = {11,22,33,44,55,88,99};
@@ -208,7 +228,7 @@ public class searchTechniques {
         // pairsOfArray();
     //    subArray();
     // subarray2();
-    maxSumSubarry();
+    kadanes(numbers);
     }
 
 }
