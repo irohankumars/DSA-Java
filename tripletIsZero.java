4b17d122e58bb778c2e60af7692405d8aa619f8e
irohankumars <kumarsrohan2@gmail.com>
2026-06-21 18:00:37 +0530

    Threesum

--- tripletIsZero.java
+++ tripletIsZero.java
@@ -1,56 +1,93 @@
+// import java.util.*;
+
+// public class tripletIsZero {
+
+//     public static List<List<Integer>> threeSum(int nums[]) {
+
+//         Set<List<Integer>> set = new HashSet<>();//is to store triplet set and to remove duplicate set
+
+//         for (int i = 0; i < nums.length; i++) {
+
+//             for (int j = i + 1; j < nums.length; j++) {
+
+//                 for (int k = j + 1; k < nums.length; k++) {
+
+//                     if (nums[i] + nums[j] + nums[k] == 0) {
+
+//                         List<Integer> triplet = new ArrayList<>();// store 1 triplet at a time
+
+//                         triplet.add(nums[i]);
+//                         triplet.add(nums[j]);
+//                         triplet.add(nums[k]);
+
+//                         Collections.sort(triplet);
+
+//                         set.add(triplet);
+//                     }
+//                 }
+//             }
+//         }
+
+//         return new ArrayList<>(set);
+//     }
+
+//     public static void main(String args[]) {
+
+//         int nums[] = {-1, 0, 1, 2, -1, -4};
+
+//         List<List<Integer>> result = threeSum(nums);
+
+//         System.out.println(result);
+//     }
+// }
+
 import java.util.*;
 
 public class tripletIsZero {
 
-    public static List<List<Integer>> threeSum(int nums[]) {
-        List<List<Integer>> result = new ArrayList<>();
+public static List<List<Integer>> threesum(int nums []){
+    
+    int n = nums.length;
 
-        Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 2; i++) {
+    Set<List<Integer>> set = new HashSet<>();
 
-            // Skip duplicate first eleents
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            for(int k=j+1; k<n; k++){
+            
+                if(nums[i]+nums[j]+nums[k] == 0){
+                    List<Integer> triplet = new ArrayList <> ();
 
-            int left = i + 1;
-            int right = nums.length - 1;
+                    triplet.add(nums[i]);
+                    triplet.add(nums[j]);
+                    triplet.add(nums[k]);
 
-            while (left < right) {
+                    Collections.sort(triplet);
 
-                int sum = nums[i] + nums[left] + nums[right];
+                    set.add(triplet);
 
-                if (sum == 0) {
-
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // Skip duplicates
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
-
-                    left++;
-                    right--;
-
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
                 }
+
             }
         }
-
-        return result;
     }
+    return new ArrayList<>(set);
 
-    public static void main(String args[]) {
+}
 
-        int nums[] = {-1, 0, 1, 2, -1, -4};
 
-        System.out.println(threeSum(nums));
-    }
+public static void main(String args[]){
+
+    int nums[] = {-1, 0, 1, 2, -1, -4};
+
+    List<List<Integer>> result = threesum(nums);
+
+    System.out.println(result);
+
+
+
+}
+
+
 }
